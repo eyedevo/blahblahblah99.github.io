915af04e0617987dbaf77b68e9d536cba3fd26ff
blahblahblah99 <136532476+blahblahblah99@users.noreply.github.com>
2026-04-10 22:43:35 +1000

    make things load

--- js/template.js
+++ js/template.js
@@ -1,11 +1,13 @@
-fetch("/templates/header.html")
-    .then(res => res.text())
-    .then(data => {
-        document.getElementById("header").outerHTML = data;
-    });
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("/templates/header.html")
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById("header").outerHTML = data;
+        });
 
-fetch("/templates/footer.html")
-    .then(res => res.text())
-    .then(data => {
-        document.getElementById("footer").outerHTML = data;
-    });
+    fetch("/templates/footer.html")
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById("footer").outerHTML = data;
+        });
+});
